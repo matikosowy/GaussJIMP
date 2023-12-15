7ffaca3a72a0c60707e1054f2f7e8636d81e2c57
Mateusz Gołębiewski <matikosowy@gmail.com>
2023-12-15 11:27:06 +0100

    Implementacja funkcji gauss.c (bez wyboru elementu glownego)

--- src/gauss.c
+++ src/gauss.c
@@ -1,14 +1,32 @@
 #include "gauss.h"
 
 /**
- * Zwraca 0 - elimnacja zakonczona sukcesem
+ * Zwraca 0 - eliminacja zakonczona sukcesem
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
  */
-int eliminate(Matrix *mat, Matrix *b){
-    /**
-  	 * Tutaj należy umieścić właściwą implemntację.
-		 */
+int eliminate(Matrix *mat, Matrix *b) {
+    // Sprawdzenie poprawności rozmiarów macierzy
+    if (mat->r != mat->c || mat->r != b->r) {
+        return 2;
+    }
 
-		return 0;
+    int i, j, k;
+    int n = mat->r;
+
+    for (k = 0; k < n - 1; k++) {
+        if (mat->data[k][k] == 0) {
+            return 1;
+        }
+
+        for (i = k + 1; i < n; i++) {
+            double factor = mat->data[i][k] / mat->data[k][k];
+
+            for (j = k; j < n; j++) {
+                mat->data[i][j] -= factor * mat->data[k][j];
+            }
+            b->data[i][0] -= factor * b->data[k][0];
+        }
+    }
+
+    return 0;
 }
-
